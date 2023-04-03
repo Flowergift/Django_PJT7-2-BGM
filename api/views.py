ff7507a804a71e0e709b160527a69f0efbe1eb63
Moody-Guy <106325381+Moody-Guy@users.noreply.github.com>
2023-04-03 19:46:05 +0900

    initial release

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+from .serializers import BookSerializer
+from .models import Book
+
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+
+
+
+# Create your views here.
